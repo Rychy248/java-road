2286220c10da3cf2857f7b2589b2eefa17629adc
Rychy248 <jorgeajrhh@gmail.com>
2025-05-19 10:11:48 -0600

    adding practice 001

---
+++ project-1/src/Hello.java
@@ -0,0 +1,25 @@
+
+public class Hello {
+    public static void main(String[] args)
+    {
+        //sayHello();
+        //testingDataTypes();
+    }
+
+    public static void sayHello(){
+        System.out.println("Hello world");
+    }
+
+    public static void testingDataTypes(){
+        char firstChar = '9';
+        String lastName = "Hi my name is XYZ 90";
+        boolean isAlive = true;
+        int livePoints;
+        double var5 = 1.2;
+
+        System.out.println(firstChar);
+        if (isAlive) {
+            System.out.println("Is alive");
+        }
+    }
+}
